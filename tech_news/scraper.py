6d1d1dc52d873b127d9a8a49568841c87cae4267
Nícolas Tozato <ntozato9@gmail.com>
2022-01-16 18:51:22 -0300

    implementa scrape_next_page_link

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -25,7 +25,11 @@ def scrape_novidades(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    result = selector.css(
+        "#js-main > div > div > div.z--col.z--w-2-3 >"
+        "div.tec--list.tec--list--lg > a::attr(href)").get()
+    return result
 
 
 # Requisito 4
@@ -40,5 +44,5 @@ def get_tech_news(amount):
 
 if __name__ == "__main__":
     main_page_html_content = fetch("https://www.tecmundo.com.br/novidades")
-    teste = scrape_novidades(main_page_html_content)
+    teste = scrape_next_page_link(main_page_html_content)
     print(teste)
